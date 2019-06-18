b41781e2ab73484c8b2961cb9f0de657ba125ef1
tangjiawen <wenzic@163.com>
2019-06-18 18:42:19 +0800

    资源引用路径调整

--- src/page/fileClassify/fileClassify.js
+++ src/page/fileClassify/fileClassify.js
@@ -169,8 +169,8 @@ function getData(page) {
           groups: groups, // 连续显示分页数
           first: "首页", // 若不显示，设置false即可
           last: "尾页", // 若不显示，设置false即可
-          prev: " ", // 若不显示，设置false即可
-          next: " ", // 若不显示，设置false即可
+            prev : '上一页', // 若不显示，设置false即可
+            next : '下一页 ', // 若不显示，设置false即可
           jump: function(obj, first) {
             // 触发分页后的回调
             if (!first) {
@@ -341,8 +341,8 @@ function getSearchData(page) {
           groups: groups, // 连续显示分页数
           first: "首页", // 若不显示，设置false即可
           last: "尾页", // 若不显示，设置false即可
-          prev: " ", // 若不显示，设置false即可
-          next: " ", // 若不显示，设置false即可
+            prev : '上一页', // 若不显示，设置false即可
+            next : '下一页 ', // 若不显示，设置false即可
           jump: function(obj, first) {
             // 触发分页后的回调
             if (!first) {
